7a633304ec7468965c4b8ef9f1021f83ceed8161
Toshiaki Maki <tmaki@pivotal.io>
2018-04-01 20:28:37 +0900

    Polish

--- helper.sh
+++ helper.sh
@@ -9,7 +9,7 @@ cat <<EOF
 ## Google Cloud Platform Config
 
 Project ID = $(echo $AUTH_JSON | jq -r .project_id)
-Default Deployment Tag = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Default Deployment Tag"].value')
+Default Deployment Tag = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Default Deployment Tag".value')
 AuthJSON = ${AUTH_JSON}
 
 ## Director Config
@@ -19,42 +19,42 @@ Enable Post Deploy Scripts  = checked
 
 ## Create Availability Zones
 
-Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Availability Zones"].value')
+Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
 
 ## Networks
 
 ### pks-infrastructure
 
-Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network Name"].value')
+Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network Name".value')
 Service Network	= unchecked
-Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network Google Network Name "].value')
-CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network CIDR"].value')
-Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network Reserved IP Ranges"].value')
-DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network DNS"].value')
-Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Infrastructure Network Gateway"].value')
-Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Availability Zones"].value')
+Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network Google Network Name ".value')
+CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network CIDR".value')
+Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network Reserved IP Ranges".value')
+DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network DNS".value')
+Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Infrastructure Network Gateway".value')
+Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
 
 ### pks-main
 
-Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network Name"].value')
+Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network Name".value')
 Service Network	= unchecked.
-Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network Google Network Name "].value')
-CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network CIDR"].value')
-Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network Reserved IP Ranges"].value')
-DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network DNS"].value')
-Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Main Network Gateway"].value')
-Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Availability Zones"].value')
+Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network Google Network Name ".value')
+CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network CIDR".value')
+Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network Reserved IP Ranges".value')
+DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network DNS".value')
+Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network Gateway".value')
+Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
 
 ### pks-services
 
 Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Service Network Name"].value')
 Service Network	= checked
-Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Service Network Google Network Name "].value')
-CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Service Network CIDR"].value')
-Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Services Network Reserved IP Ranges"].value')
-DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Services Network DNS"].value')
-Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs["Services Network Gateway"].value')
-Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Availability Zones"].value')
+Google Network Name = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Service Network Google Network Name ".value')
+CIDR = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Service Network CIDR".value')
+Reserved IP Ranges = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Services Network Reserved IP Ranges".value')
+DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Services Network DNS".value')
+Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Services Network Gateway".value')
+Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
 
 
 EOF
