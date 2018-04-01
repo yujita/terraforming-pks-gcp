d8a44aed47c1970130f1c62b8c77689fd8fffe04
Toshiaki Maki <tmaki@pivotal.io>
2018-04-02 00:50:55 +0900

    Update helper

--- helper.sh
+++ helper.sh
@@ -56,5 +56,31 @@ DNS	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Services Network DNS
 Gateway	= $(cat terraform.tfstate | jq -r '.modules[0].outputs."Services Network Gateway".value')
 Availability Zones = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
 
+# Pivotal Container Service
 
+## AZ and Network Assignments
+
+Place singleton jobs = Any AZ
+Balance other jobs = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Availability Zones".value')
+Network = $(cat terraform.tfstate | jq -r '.modules[0].outputs."Main Network Name".value')
+Service Network = $(cat terraform.tfstate | jq -r '.modules[0].outputs["Service Network Name"].value')
+
+## PKS API Service
+
+Generate RSA Certificate = *.pks.example.com
+
+## Kubernetes Cloud Provider Configuration
+
+Choose your IaaS = GCP
+GCP Project ID = $(echo $AUTH_JSON | jq -r .project_id)
+VPC Network = $(cat terraform.tfstate | jq -r '.modules[0].resources."google_compute_network.pks-network".primary.id')
+GCP Service Key = ${AUTH_JSON}
+
+# UAA Configuration
+
+UAA URL = api.pks.example.com
+
+# Resource Config
+
+LOAD BALANCERS = tcp:$(cat terraform.tfstate | jq -r '.modules[0].resources."google_compute_target_pool.pks-api".primary.id')
 EOF
